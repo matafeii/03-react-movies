c112cc0c7bb6e389e06f0625e3d11c77ffd3f8e1
Kit_Kat <matafeii38@gmail.com>
2026-04-15 09:38:56 +0300

    redacted_js_main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
-import './style.css'
+import 'modern-normalize';
+import './style.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
+import App from './components/App/App';
+
+const root = document.getElementById('app');
+
+if (!root) {
+  throw new Error('Root element not found');
+}
+
+createRoot(root).render(
+  <StrictMode>
+    <App />
+    <Toaster position="top-right" />
+  </StrictMode>
+);
